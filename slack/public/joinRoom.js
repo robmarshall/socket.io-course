db55c67a914f5b8ba02ebcdc56af908a41dbf60d
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-04-13 17:21:57 +0100

    Added basic search

--- slack/public/joinRoom.js
+++ slack/public/joinRoom.js
@@ -5,7 +5,6 @@ function joinRoom(roomName) {
         document.querySelector(
             '.curr-room-num-users-count'
         ).innerHTML = newNumberOfUsers
-        document.querySelector('.curr-room-text').innerText = `${roomName}`
     })
     nsSocket.on('historyCatchUp', history => {
         console.log(history)
@@ -22,5 +21,25 @@ function joinRoom(roomName) {
         document.querySelector(
             '.curr-room-num-users-count'
         ).innerHTML = numMembers
+        document.querySelector('.curr-room-text').innerText = `${roomName}`
+    })
+
+    let searchBox = document.querySelector('#search-box')
+    searchBox.addEventListener('input', e => {
+        let messages = Array.from(
+            document.getElementsByClassName('message-text')
+        )
+        messages.forEach(msg => {
+            if (
+                msg.innerText
+                    .toLowerCase()
+                    .indexOf(e.target.value.toLowerCase()) === -1
+            ) {
+                // The message does not contain search term
+                msg.style.display = 'none'
+            } else {
+                msg.style.display = 'block'
+            }
+        })
     })
 }
