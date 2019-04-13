18c8f59433e0cb90cb9c4491ea9621c18c8615a3
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-04-13 17:22:18 +0100

    Add/remove event listeners

--- slack/public/joinNs.js
+++ slack/public/joinNs.js
@@ -1,4 +1,12 @@
 function joinNs(endpoint) {
+    if (nsSocket) {
+        // Check to see if nsSocket is a socket
+        nsSocket.close()
+        // remove the event listener before it is added again
+        document
+            .querySelector('#user-input')
+            .removeEventListener('submit', formSubmittion)
+    }
     nsSocket = io(`http://localhost:9000${endpoint}`)
     nsSocket.on('nsRoomLoad', nsRooms => {
         let roomList = document.querySelector('.room-list')
@@ -17,7 +25,9 @@ function joinNs(endpoint) {
         // add click listener to each room
         let roomNodes = document.getElementsByClassName('room')
         Array.from(roomNodes).forEach(elem => {
-            elem.addEventListener('click', e => {})
+            elem.addEventListener('click', e => {
+                joinRoom(e.target.innerText)
+            })
         })
 
         // Add room automatically - first time here
@@ -33,11 +43,13 @@ function joinNs(endpoint) {
 
     document
         .querySelector('.message-form')
-        .addEventListener('submit', event => {
-            event.preventDefault()
-            const newMessage = document.querySelector('#user-message').value
-            nsSocket.emit('newMessageToServer', { text: newMessage })
-        })
+        .addEventListener('submit', formSubmittion)
+}
+
+function formSubmittion(event) {
+    event.preventDefault()
+    const newMessage = document.querySelector('#user-message').value
+    nsSocket.emit('newMessageToServer', { text: newMessage })
 }
 
 function buildHTML(msg) {
