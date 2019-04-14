cb26c2c8a7e35104e635e5f6b0bdaad0119d77e1
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-04-14 13:32:38 +0100

    Added player and mouse logic

---
+++ agarclone/public/canvasStuff.js
@@ -0,0 +1,64 @@
+function init() {
+    draw()
+}
+
+//=========== Drawing ===========
+let randomX = Math.floor(500 * Math.random() + 10)
+let randomY = Math.floor(500 * Math.random() + 10)
+function draw() {
+    context.beginPath()
+    context.fillStyle = 'rgb(255,0,0)'
+    // arg1,2 = x,y of the arc
+    // arg3 = radius
+    // arg4 = where the circle starts
+    // arg5 = where the circle stops
+    context.arc(randomX, randomY, 10, 0, Math.PI * 2)
+    context.fill()
+    context.lineWidth = 3
+    context.strokeStyle = 'rgb(0,255,0)'
+    context.stroke()
+    requestAnimationFrame(draw)
+}
+
+canvas.addEventListener('mousemove', event => {
+    const mousePosition = {
+        x: event.clientX,
+        y: event.clientY,
+    }
+    const angleDeg =
+        (Math.atan2(
+            mousePosition.y - canvas.height / 2,
+            mousePosition.x - canvas.width / 2
+        ) *
+            180) /
+        Math.PI
+    if (angleDeg >= 0 && angleDeg < 90) {
+        xVector = 1 - angleDeg / 90
+        yVector = -(angleDeg / 90)
+    } else if (angleDeg >= 90 && angleDeg <= 180) {
+        xVector = -(angleDeg - 90) / 90
+        yVector = -(1 - (angleDeg - 90) / 90)
+    } else if (angleDeg >= -180 && angleDeg < -90) {
+        xVector = (angleDeg + 90) / 90
+        yVector = 1 + (angleDeg + 90) / 90
+    } else if (angleDeg < 0 && angleDeg >= -90) {
+        xVector = (angleDeg + 90) / 90
+        yVector = 1 - (angleDeg + 90) / 90
+    }
+
+    speed = 10
+    xV = xVector
+    yV = yVector
+
+    if (
+        (player.locX < 5 && player.xVector < 0) ||
+        (player.locX > 500 && xV > 0)
+    ) {
+        player.locY -= speed * yV
+    } else if ((player.locY < 5 && yV > 0) || (player.locY > 500 && yV < 0)) {
+        player.locX += speed * xV
+    } else {
+        player.locX += speed * xV
+        player.locY -= speed * yV
+    }
+})
