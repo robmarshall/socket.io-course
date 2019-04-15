620ccefe4a5d4716cbb2671de387b42856d9f648
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-04-15 08:30:40 +0100

    Added player class

--- agarclone/sockets/classes/Player.js
+++ agarclone/sockets/classes/Player.js
@@ -1,1 +1,10 @@
 // This is where all the data is stored for a player
+class Player {
+    constructor(socketId, playerConfig, playerData) {
+        this.socketId = socketId
+        this.playerConfig = playerConfig
+        this.playerData = playerData
+    }
+}
+
+module.exports = Player
