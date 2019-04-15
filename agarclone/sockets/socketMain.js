73441a3307f48825d6e5e4827552ab53e846325e
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-04-15 08:16:49 +0100

    Added settings

--- agarclone/sockets/socketMain.js
+++ agarclone/sockets/socketMain.js
@@ -3,6 +3,9 @@ const io = require('../servers').io
 
 const Orb = require('./classes/Orb')
 let orbs = []
+let settings = {
+    defaultOrbs: 500,
+}
 
 initGame()
 
@@ -23,7 +26,7 @@ io.sockets.on('connect', socket => {
 
 // Run at begining of new game
 function initGame() {
-    for (let i = 0; i < 500; i++) {
+    for (let i = 0; i < settings.defaultOrbs; i++) {
         orbs.push(new Orb())
     }
 }
