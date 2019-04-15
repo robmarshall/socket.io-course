33ff8a1efa2ce2c072e6308a073a9119a9729e92
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-04-15 08:29:04 +0100

    Added general player config

--- agarclone/sockets/classes/PlayerConfig.js
+++ agarclone/sockets/classes/PlayerConfig.js
@@ -1,1 +1,11 @@
 // This is where all the data is stored that no other player needs
+class PlayerConfig {
+    contructor(settings) {
+        this.xVector = 0
+        this.yVector = 0
+        this.speed = settings.defaultSpeed
+        this.zoom = settings.defaultZoom
+    }
+}
+
+module.exports = PlayerConfig
